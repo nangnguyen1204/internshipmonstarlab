f275ac5519479deae729181605231e41181aecd5
congnang1204 <congnang1242000@gmail.com>
2022-02-22 00:21:02 +0700

    21/2

--- TaskJS/task.js
+++ TaskJS/task.js
@@ -1,12 +1,14 @@
 //Basic
 //Cau1
+
+console.log("Basic");
 function random(start , end){
     return Math.round(Math.random() * (end - start) + start);
 }
 console.log(random(6,10));
 
 //Cau2
-var init = {};
+var init = [];
 for(i=0; i<10 ; i++){
     init[i] = random(5,20);
 }
@@ -31,89 +33,183 @@ console.log(duplicated);
 const arr1 = [1 , 3, 4, 5, 1, 3, 1];
 let newarr= {};
 arr1.reduce((pre, curr) => {
-if(Object.keys(newarr).length == 0) {
-    newarr[curr] = 1;
-} else {
-    for(let i = 0; i < Object.keys(newarr).length; i++) {
-                  
-        if(Object.keys(newarr)[i] == curr){
-            newarr[Object.keys(newarr)[i]] += 1;
-                break;
-        } else if(i == Object.keys(newarr).length - 1) {
-            newarr[curr] = 1;
-            break;
-            }
-        }
+    if(newarr.hasOwnProperty(curr)){
+        newarr[curr] += 1;
+    } else {
+        newarr[curr] = 1
     }
 }, 0)
 console.log(newarr);
 
 //Advance
+console.log("Advance");
 //Cau1
 function getRandomMark(start, end, step) {
 
     return (Math.round(Math.random() * ((end - start)/step) + (start)/step))*step;
 }
 console.log(getRandomMark(6,10,0.5));
+//Cau 2
+function generateStudentMark(name){
+    var obj={
+      name: name,
+      marks:{
+        literature:getRandomMark(6,10,0.5),
+        math:getRandomMark(6,10,0.5),
+        chemistry:getRandomMark(6,10,0.5),
+        history:getRandomMark(6,10,0.5),
+        biology:getRandomMark(6,10,0.5)
+      }
+    }
+     return obj
+}
+ 
+const markList = [
+    generateStudentMark("nang"),
+    generateStudentMark("ngoc"),
+    generateStudentMark("khanh"),
+    generateStudentMark("hoa"),
+    {
+        name: "nhung",
+        marks: {
+            literature: 9,
+            maths: 8,
+            chemistry: 7,
+            history: 9,
+            biology: 9,
+            english: 9,
+            java: 10
+        }
+    }
+]
 
-var markStudent0 = {
-    name: "Nguyễn Văn A",
-    marks:{
-        literature: getRandomMark(6,10,0.5),
-        maths: getRandomMark(6,10,0.5),
-        chemistry: getRandomMark(6,10,0.5),
-        history: getRandomMark(6,10,0.5),
-        biology: getRandomMark(6,10,0.5)
+function calculator(student){
+    const countMarks = Object.keys(student.marks).length;
+    let totalScore = 0;
+    for(const key in student.marks){
+        if(key == "literature" || key == "maths"){
+            totalScore += student.marks[key] *2;
+        }else {
+            totalScore += student.marks[key]
+        }
     }
-}
-var markStudent1 = {
-    name: "Nguyễn Văn B",
-    marks:{
-        literature: getRandomMark(6,10,0.5),
-        maths: getRandomMark(6,10,0.5),
-        chemistry: getRandomMark(6,10,0.5),
-        history: getRandomMark(6,10,0.5),
-        biology: getRandomMark(6,10,0.5)
-    }
-}
-var markStudent2 = {
-    name: "Nguyễn Văn C",
-    marks:{
-        literature: getRandomMark(6,10,0.5),
-        maths: getRandomMark(6,10,0.5),
-        chemistry: getRandomMark(6,10,0.5),
-        history: getRandomMark(6,10,0.5),
-        biology: getRandomMark(6,10,0.5)
-    }
-}
-var markStudent3 = {
-    name: "Nguyễn Văn D",
-    marks:{
-        literature: getRandomMark(6,10,0.5),
-        maths: getRandomMark(6,10,0.5),
-        chemistry: getRandomMark(6,10,0.5),
-        history: getRandomMark(6,10,0.5),
-        biology: getRandomMark(6,10,0.5)
-    }
-}
-var markStudent4 = {
-    name: "Nguyễn Văn E",
-    marks:{
-        literature: getRandomMark(6,10,0.5),
-        maths: getRandomMark(6,10,0.5),
-        chemistry: getRandomMark(6,10,0.5),
-        history: getRandomMark(6,10,0.5),
-        biology: getRandomMark(6,10,0.5)
+    let averageScore = totalScore / (countMarks +2);
+    return{
+        name: student.name,
+        averageScore: averageScore
     }
 }
 
-console.log(markStudent0);
+function bonus(student){
+    let total= 0;
+    for(const key in student.marks){
+        if(student.marks[key] == 8) {
+            total += 1000000; 
+        } else if(student.marks[key] == 9) {
+            total += 2000000; 
+        } else if(student.marks[key] ==10) {
+            total += 5000000; 
+        }
+    }
+    return {
+        name: student.name,
+        bonus: total
+    };
+}
 
-var markList = [];
-markList[0] = markStudent0;
-markList[1] = markStudent1;
-markList[2] = markStudent2;
-markList[3] = markStudent3;
-markList[4] = markStudent4;
+let result = [];
+let listStudent = [];
+let literatureMax = 0;
+let mathsMax = 0;
+let listBonus = [];
+markList.map(student => {
+    result.push(calculator(student))
 
-console.log(markList);
+    //1 điểm lớn nhất môn văn
+    if(student.marks.literature > literatureMax){
+        literatureMax = student.marks.literature
+    }
+    
+    //2 điểm lớn nhất môn toán
+    if(student.marks.maths > mathsMax){
+        mathsMax = student.marks.maths
+    }
+
+
+    // caculator bonus
+    listBonus.push(bonus(student))
+    
+})
+
+// search list student have average score bigger 8
+result.map(student => {
+    if(student.averageScore > 8){
+        return listStudent.push(student);
+    }
+})
+
+
+
+// list student  (average score)
+console.log("Cau 4")
+console.log(result)
+// tiền thưởng
+console.log("Cau 6")
+console.log(listBonus)
+
+// student  literature has score max
+console.log("Diem van cao nhat")
+console.log(literatureMax)
+
+// student math has score max
+console.log("Diem toan cao nhat")
+console.log(mathsMax)
+
+// list student has average bigger 8
+console.log("danh sach sinh vien trung binh tren 8")
+console.log(listStudent)
+
+
+//Cau7 
+ const obj_1 = {name : "nam" , age : 20};
+ const obj_2 = JSON.parse(JSON.stringify(obj_1));
+ console.log(obj_2)
+
+//Cau8
+function isObject(obj) {
+    return obj != null && typeof obj === "object";
+}
+function isDeepEqual(obj1, obj2) {
+    const keys1 = Object.keys(obj1); 
+    const keys2 = Object.keys(obj2); 
+  
+    // so sánh số lượng keys
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (const key of keys1) {
+      const val1 = obj1[key];
+      const val2 = obj2[key];
+  
+      // kiểm tra xem hai giá trị có cùng là object hay không
+      const areObjects = isObject(val1) && isObject(val2);
+  
+      // nếu cùng là object thì phải gọi đệ quy để so sánh 2 object
+      if (areObjects && !isDeepEqual(val1, val2)) {
+        return false;
+      }
+  
+      // nếu không cùng là object thì so sánh giá trị
+      if (!areObjects && val1 !== val2) {
+        return false;
+      }
+    }
+  
+    return true;
+}
+  
+let point1 = { x: 1, y: 2, metadata: { type: "point" } };
+let point2 = { x: 1, y: 2, metadata: { type: "point" } };
+  
+console.log(isDeepEqual(point1, point2)); 
